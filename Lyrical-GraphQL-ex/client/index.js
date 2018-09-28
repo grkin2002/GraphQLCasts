3f6ffad2be8f5cbdd4f9b0afe366238e5ba12d59
greg pong <grkin2002@gmail.com>
2018-09-28 14:06:13 +1000

    ClientSide GraphQL finished

--- Lyrical-GraphQL-ex/client/index.js
+++ Lyrical-GraphQL-ex/client/index.js
@@ -1,18 +1,14 @@
-import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+
 import App from './components/App';
 import SongList from './components/SongList';
-import SongCreate from './components/SongCreate';
-import SongDetail from './components/SongDetail';
 
-const client = new ApolloClient({
-  dataIdFromObject: o => o.id
-});
+const client = new ApolloClient({});
 
 const Root = () => {
   return (
@@ -20,15 +16,10 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
-          <Route path="songs/new" component={SongCreate} />
-          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
